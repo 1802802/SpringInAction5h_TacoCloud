d8f12461e21e5d6fdf2a7f48e0503b50d8e5ce59
1802802 <35828295+1802802@users.noreply.github.com>
2021-08-01 00:42:53 +0800

    Initial commit

---
+++ src/main/java/spring/in/action/fifth/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,15 @@
+package spring.in.action.fifth.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/in/action/fifth/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,15 @@
+package spring.in.action.fifth.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
